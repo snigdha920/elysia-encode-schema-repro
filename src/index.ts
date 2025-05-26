f203dbb694f8112af544d16d79eb3e55a38f763f
Snigdha Singh <snigdha920@gmail.com>
2025-05-26 12:11:07 +0200

    fix response type in the reproduction

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 import { t } from "elysia/type-system";
+import { Address, getAddress } from "viem";
 
-export const StringifiedBigInt = () =>
+const StringifiedBigInt = () =>
   t
     .Transform(t.String())
     .Decode((value) => BigInt(value))
@@ -9,31 +10,107 @@ export const StringifiedBigInt = () =>
       return value.toString();
     });
 
+const EthereumAddress = () =>
+  t
+    .Transform(t.String())
+    .Decode((value) => {
+      return getAddress(value);
+    })
+    .Encode((value) => {
+      return value;
+    });
+
+const TOKENS = [
+  {
+    id: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" as Address,
+    totalSupply: BigInt(1),
+    internalId: "1",
+  },
+  {
+    id: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8" as Address,
+    totalSupply: BigInt(2),
+    internalId: "2",
+  },
+];
+
 const app = new Elysia({
   aot: true,
   encodeSchema: true,
 })
   .get("/", () => "Hello Elysia")
   .get(
+    "/tokens",
+    () => {
+      return TOKENS;
+    },
+    {
+      response: {
+        // BROKEN: Intersect + more than 1 Transform does not work, error: "JSON.stringify cannot serialize BigInt."
+        200: t.Array(
+          t.Intersect([
+            t.Object({
+              id: EthereumAddress(),
+              totalSupply: StringifiedBigInt(),
+            }),
+            t.Object({
+              id: EthereumAddress(),
+              internalId: t.String(),
+            }),
+          ])
+        ),
+      },
+    }
+  )
+  .get(
+    "/tokens3",
+    () => {
+      return TOKENS;
+    },
+    {
+      response: {
+        // WORKS: Intersect + only 1 transform type
+        200: t.Array(
+          t.Intersect([
+            t.Object({
+              id: t.String(),
+              totalSupply: StringifiedBigInt(),
+            }),
+            t.Object({
+              id: t.String(),
+              internalId: t.String(),
+            }),
+          ])
+        ),
+      },
+    }
+  )
+  .get(
+    "/tokens2",
+    () => {
+      return TOKENS;
+    },
+    {
+      response: {
+        // WORKS: No Intersect + more than 1 transform type
+        200: t.Array(
+          t.Object({
+            id: EthereumAddress(),
+            totalSupply: StringifiedBigInt(),
+          })
+        ),
+      },
+    }
+  )
+  .get(
     "/bigint",
     () => {
       return BigInt(1);
     },
     {
-      200: StringifiedBigInt(), // Expected: "1", received: response is not an Object. (evaluating '"charCodeAt"in response')
-    }
-  )
-  .get(
-    "/bigint2",
-    () => {
-      return {
-        bigint: BigInt(1),
-      };
-    },
-    {
-      200: t.Object({
-        bigint: StringifiedBigInt(), // Expected: {bigint: "1"}, received: JSON.stringify cannot serialize BigInt.
-      }),
+      response: {
+        // Simple Transform works
+        200: StringifiedBigInt(),
+      },
     }
   )
   .listen(3000);
